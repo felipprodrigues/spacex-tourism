41527cf6c7af0e413875853e7c8ef076eba0083e
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2021-10-28 01:33:15 -0300

    include a few testing elements

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="hero">
+        <p className="no-space-out">Hello world</p>
+      </div>
+    )
+  }
+}
+
+export default App;
