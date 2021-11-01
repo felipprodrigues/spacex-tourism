0ce2ee1c933d0861463388b5a55438b54d7fe5cd
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2021-10-31 21:10:58 -0300

    add new html element to navbar

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
     return (
       <div className="hero">
         <div className="hero__navbar navbar">
+          <hr className="navbar__line--horizontal"></hr>
           <Navbar />
         </div>
       </div>
